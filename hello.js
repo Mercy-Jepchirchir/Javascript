19ec6fbfd5aae6399b91157522dcadc3f7c0ead6
Mercy-Jepchirchir <mjepchirchirkemboi@gmail.com>
2022-06-13 21:06:25 +0300

    first commit

---
+++ hello.js
@@ -0,0 +1,64 @@
+//  var x = 10;
+//  var y = 20;
+//  var z = y-x
+//  console.log(z)
+//  document.write(z)
+
+// //  document.getElementById("button").addEventListener('click', function(){
+
+// //  )}
+// var name = "conslate";//global variable
+
+
+// function me(){
+//     var car = "Toyota";//local variable
+//     console.log(name)
+// }
+// me()
+// console.log(car)
+
+// function practice(){
+//     let lady = 45
+// }
+// const children = 34;
+// children = 50;
+// console.log(children)//error because const is immutable
+// var a = null;
+// var b = undefined;// inbuilt function
+// console.log(typeof a)
+
+// var a;//undefined
+// a = 4
+// console.log(typeof a)// changes to a number
+
+// //arrays is a collection of elements
+// let num = [1,2,"3", [4]];
+// console.log(num.length);
+// console.log(num[3][0]);//2d arrays
+
+// //adding an element inside an Array
+// num [4] = 20
+// console.log(num);
+
+// let a = "foo";
+// let b= a.concat("bar")
+// console.log(b)
+// class Pupil {
+//     constructor (name){
+//         this.name = name
+//     }
+// }
+
+// Pupil = new Pupil
+var a = "34";
+Number(a);
+console.log(Number(a)+3)//explicit coercion
+console.log(typeof(a));
+let d = ""
+console.log(Number(d));
+
+// let b = 4;
+// let c = 4 + "hey";// implicit coercion
+// console.log(C);
+// console.log(typeof(c));
+
